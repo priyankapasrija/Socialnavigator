80d7bf75310ec19e7b81d437d9b9e0c1ee3ee40f
Harun Kohen <110021233+kohkohshine@users.noreply.github.com>
2024-09-10 18:39:17 +0200

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -9,6 +9,7 @@ export const fetchSimulatedReview = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/api/reviews/simulated-review`);
     return response.data;
+      console.log(chalk.green(`Simulated Review is fetched🥰’));
   } catch (error) {
     console.error('Error fetching the simulated review:', error);
     throw error;
